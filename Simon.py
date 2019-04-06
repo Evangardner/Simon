2efd6107e011ebe6d1be73dc6e63c2ccc4f5af47
evangardner <evanmgardner@gmail.com>
2019-04-05 20:32:29 -0400

    second commit

--- Simon.py
+++ Simon.py
@@ -1,11 +1,35 @@
-class Node:
-   
+def bathroom():
+    if inputVal=="sink":
+	#do sink things
+    elif inputVal=="shower":
+        #do shower things
+    elif inputval=="go to Hallway":
+        #return to hallway
+    else:
+	print("invalid choice")
+
+def bedroom():
+    if inputVal=="bed":
+	#do bed things
+    elif inputVal=="nightstand":
+        #do nightstand things
+    elif inputVal=="dresser":
+        #dresser things
+    elif inputVal=="closet":
+        #leave bathroom, set room to hallway
+    elif inputVal=="go to Hallway":
+        #return to hallway
+    else:
+	print("invalid choice")
 
 
-
-
-
-
-
-def run():
     
+
+
+
+
+
+
+
+print("you wake up in your room")
+
