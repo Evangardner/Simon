1aecf6fdd09154e59d536b0c8549f52bd413862b
Maxwellgm1 <maxwellgm1@appstate.edu>
2019-04-06 01:19:12 -0400

    completed more rooms

--- Simon.py
+++ Simon.py
@@ -1,27 +1,48 @@
 def kitchen():
     print "You enter the Kitchen and see a Fridge, Stove, and Table ... "
 
-    inputVal = input("Your choices are:\n1.Fridge\nStove\n3.Table\n4.Return to Hallway")
+    inputVal = input("Your choices are:\n1.Fridge\n2.Stove\n3.Table\n4.Return to Hallway")
     if inputVal==1:
-	print "You look at the fridge, you stomach grumbles ... you are hungry"
+	    print "You look at the fridge, you stomach grumbles ... you are hungry"
     elif inputVal==2:
-        print "You look at the stove, there is a letter sitting on top ... who left this? Sophia?"
-#add in stuff for reading letter twice etc ... ADD COUNTER
+        print "You look at the stove, there is a letter sitting on top ..."
+#add in stuff for reading letter twice etc ... ADD COUNTER, for first letter, write some specific statements etc
     elif inputVal==3:
         print "You look at the table, yesterdays dirty dishes are left. Sophia has been rather absent recently ..."
     elif inputVal==4:
         return
     else:
- 	print"invalid choice"
+ 	    print"invalid choice"
 
 def garage():
-    print""
+    print"You enter the Garage, the concrete is cool on your feet. You see that Sophia's car is missing, where did she go?"
+    
+    inputVal = input("Your choices are:\n1.Look through the window\n2.Return to Hallway")
+    if inputVal==1:
+        print "You look through the window, there are no cars in sight. It is a sunny and beautiful day ..."
+        print "you wish you could go outside for a bit ..."
+    elif inputVal==2:
+        return
+    else:
+        print"invalid choice"
 
 def window():
-    print""
+    print"You peek through the window, a bird flys by and you are distracted for a period of time ... "
+#look through the window a second time and dont see bird D:
 
 def livingRoom():
-    print""
+    print"You enter the living room and see a TV, Couch, and Coffee Table"
+    inputVal = input("Your choices are:\n1.TV\n2.Couch\n3.Coffee Table\n4.Return to Hallway")
+    if inputVal==1:
+        print "The TV is on, soothing music is playing."
+    elif inputVal==2:
+        print "You see a letter stuffed between the cushions... "
+    elif inputVal==3:
+        print "You look on the coffee table, there is a half finished cup of tea ... Sophia always finishes her tea"
+    elif inputVal==4:
+        return
+    else:
+        print"invalid choice"
 
 def bathroom():
     inputVal = input("Select choice")
