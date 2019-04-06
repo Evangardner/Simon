b73d392b3032a2ac5a9855d9c446f5f6116716ff
evangardner <evanmgardner@gmail.com>
2019-04-05 21:43:01 -0400

    finished main

--- Simon.py
+++ Simon.py
@@ -32,12 +32,14 @@ def hallway():
         print "you are in the hallway"
         print "your choices are:\n1.Bedroom\n2.Bathroom\n3.Kitchen\n4.Garage\n5.Window\n6.Living Room"
 	inputVal = input("\nEnter your choice:")
+
 print"-----------------------------------------"
 print"            Wake up Simon"
 print"-----------------------------------------"
 print"    you wake up in the hallway. why did you fall asleep here? you stand up and stretch."
-print"Where is Sophia?  
+print"Where is Sophia? you need to find out where she went. better investigate.              "
 hallway()
+
 
 
 
