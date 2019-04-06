f11f8680dccf42ef0138506b61c5a8153df23b6a
Maxwellgm1 <maxwellgm1@appstate.edu>
2019-04-06 05:28:38 -0400

    :) even closer, ASCII Art in!!

--- Simon.py
+++ Simon.py
@@ -1,12 +1,40 @@
 def kitchen(): 
-    print "You enter the Kitchen and see a Fridge, Stove, and Table ... "
+    print "In the Kitchen you see a Fridge, Stove, and Table ... "
     bool = True
     while True:
         inputVal = input("Your choices are:\n1.Fridge\n2.Stove\n3.Table\n4.Return to Hallway\nSelect choice: ")
         if inputVal==1:
-	        print "\nYou look at the fridge, you stomach grumbles ... you are hungry"
+            print "       ___________,_____"
+            print "      |     |  #  |=====|"
+            print "      |     | (_) |=====|"
+            print "      |> _  |_____|=====|"
+            print "      | [_] |     |     |"
+            print "      |     |_____|=====|"
+            print "      |     |     |_____|"
+            print "      |   ] |_____|     |"
+            print "      |     |_____|=====|"
+            print "      |     | ___ |_____|"
+            print "      |>    |[___]|     |"
+            print "      |     |[___]|=====|"
+            print "      |_____|=====|_____|"
+            print "\nYou look at the fridge, you stomach grumbles ... you are hungry"
         elif inputVal==2:
             if bool:
+                print "                 _.-------------."
+                print "             .-''            .;'|"
+                print "            ;==============;+'  |"
+                print "            |              |    |"
+                print "            | (} (} {) (}  |    |"
+                print "            |              |    | "
+                print "            | .==========. |    |"
+                print "            | | _ .''+_/)| |    |"
+                print "            | |( \(  (`( | |    |  "
+                print "            | | \- `.  -)| |    |"
+                print "            | | ( (  _  )| |    |"
+                print "            | |  `--' `' | |    ;"
+                print "            | `----------' |  .'"
+                print "            |              |.'"
+                print "            `--------------'  "
                 print "\nYou look at the stove, there is a letter sitting on top ..."
                 letter(letterCount)
                 global letterCount
@@ -16,6 +44,17 @@ def kitchen():
 
 #add in stuff for reading letter twice etc ... ADD COUNTER, for first letter, write some specific statements etc
         elif inputVal==3:
+            print "                     ,%;,"
+            print "                     ,%%,"
+            print "        ______________)(______________"
+            print "       /    \(__)    (__)             \ "
+            print "      /________________________________\ "
+            print "         \ \  / /            \ \  / /"
+            print "          \ \/ /              \ \/ /"
+            print "          _\/ /________________\ \/_"
+            print "         [_/o/__________________\o\_]"
+            print "          / /\ \              / /\ \ "
+            print "         /_/  \_\            /_/  \_\ "
             print "\nYou look at the table, yesterdays dirty dishes are left. Sophia has been rather absent recently ..."
         elif inputVal==4:
             return
@@ -35,10 +74,23 @@ def garage():
             print"\nInvalid choice\n"
 
 def window():
-    bool = True
-    if bool:
+    global boolGlobal
+    if boolGlobal:
+        print "    o=(=(=(=(=(=(=)=)=)=)=o"
+        print "     !-'-'-'-/_\-'-'-'-! "
+        print "     ! !  , /___\`  ! !!"
+        print "     !!! , /  |  \`  ! !"
+        print "     !!  ,|___|___`  !!!"
+        print "     !_,| |_______|` `_!"
+        print "     !-`| |       | |,-!"
+        print "     !!!! |       | ! !!"
+        print "     !!!! |       | !!!!"
+        print "     !!!!_|_______|_!!!!"
+        print "     !!!!___________!!!! "
+        print "     !!!!           !!!!  "
+        print "     !!!!           !!!!"
         print "You peek through the window, a bird flys by and you are distracted for a period of time ... "
-        bool = False
+        boolGlobal = False
     else: print "You peek through the window again, sadly there's no bird this time ..."
 
 def livingRoom():
@@ -47,9 +99,29 @@ def livingRoom():
     while True:
         inputVal = input("Your choices are:\n1.TV\n2.Couch\n3.Coffee Table\n4.Return to Hallway\nSelect choice: ")
         if inputVal==1:
+            print "      \\     // "
+            print "       \\   //"
+            print "        \\ // "
+            print "       /~~~~~\ "
+            print ",-------------------,"
+            print "| ,---------------, |"
+            print "| |               | |"
+            print "| |               | |"
+            print "| |               | |"
+            print "| |               | |"
+            print "| |               | |"
+            print "|___________________|"
+            print "|___________________|"
             print "The TV is on, soothing music is playing."
         elif inputVal==2:
             if bool:
+                print "     .------------------."
+                print "     | . . . . . . . . .|"
+                print "     | .'.'.'.'.'.'.'.'.|"
+                print "    ()______ ____ ______()"
+                print "    ||______I____I______||"
+                print "    ||__________________||"
+                print "     W                  W "
                 print "You see a letter stuffed between the cushions... "
                 letter(letterCount)
                 global letterCount
@@ -57,6 +129,13 @@ def livingRoom():
                 bool = False
             else: print "You see the couch with the letter you already read ..."
         elif inputVal==3:
+            print "        _______________________"
+            print "       /                        \ "
+            print "      /__________________________\ "
+            print "      [__________________________]"
+            print "       | |                    | |"
+            print "       | |                    | |"
+            print "        w                      w"
             print "You look on the coffee table, there is a half finished cup of tea ... Sophia always finishes her tea"
         elif inputVal==4:
             return
@@ -69,9 +148,33 @@ def bathroom():
     while True:
         inputVal = input("Your choices are:\n1.Sink\n2.Bathtub\n3.Return to Hallway\nSelect choice: ")
         if inputVal==1:
-	        print "You look at the sink, and see your reflection in the mirror. You look cute today :3"
+            print "              _    __    _ "
+            print "             /_\  /  \  /_\  "
+            print "             =|= | // | =|=  "
+            print "              !   \__/   !  "
+            print "                    _       "
+            print "                   //'     "
+            print "               __T_||_T__  "
+            print "              [__________] "
+            print "               \_      _/ "
+            print "                 \    / "
+            print "                  |  | "
+            print "                  |  | "
+            print " _________________|  |________"
+            print "                 (____)      "
+            print "               .;;;;;;;;.  "
+            print "              :;;;;;;;;;;:"
+            print "               '::::::::'"
+            print "You look at the sink, and see your reflection in the mirror. You look cute today :3"
         elif inputVal==2:
             if bool:
+                print "Oo (--------------------------)"
+                print "    \                        /    o"
+                print "     \                      /    _   ."
+                print "      \          _         /    (_)"
+                print "   o  `-. .----<(o)_--. .-'"
+                print "   ----(_/------(_<_/--\_)---"
+
                 print "You look at the bathtub and dip your foot in the water, it is cold and you are now upset"
                 print "to the side of the tub you see a letter ... "
                 letter(letterCount)
@@ -87,6 +190,18 @@ def bathroom():
  	        print"\nInvalid choice\n"
 
 def bedroom():
+    print "          !__________!    .    _______"
+    print "          |____  ____|   /_\   |__*__|"
+    print "          {____}{____}  __|__  |__*__|"
+    print " _________%%%%%%%%%%%%__|_*_|__|__*__|"
+    print "         %%%%%%%%%%%%%% |   |  |/   \|"
+    print "        %%%%%%%%%%%%%%%%"
+    print "       %%%%%%%%%%%%%%%%%%"
+    print "      %%%%%%%%%%%%%%%%%%%%"
+    print "     /||||||||||||||||||||\ "
+    print "     |||||||||||||||||||||| "
+
+
     print "In the bedroom you see a Bed, Dresser, and Nightstand. It's chilly in here."
     bool = True
     while True:
@@ -152,13 +267,18 @@ def nap():
 
 def letter(letter): 
     if letter == 1:
-        print "Letter #1"
+        print "Dear Sophia,\nI had an amazing time with you today. I am so glad that we were\nable to connect. I would love nothing more than to see you again in the\nnear furture ...\nWarmly,\nKarl"
+        print "*KARL! Who is this KARL. I must find out more information ... but why would Sophia hide\nthis from me?*"
     elif letter == 2:
-        print "Letter #2"
+        print "Dearest Sophia,\nI am so happy that we were able to meet up again! I am so grateful for your presence.\nYou are the love that came without warning; you had my heart before I could say no!\nLove,\nKarl"
+        print "*They met up again! My hear is breaking ... Sophia ...*"
     elif letter == 3:
-        print "Letter #3"
+        print "Sophia!\nNo mountain, nor sea, no thing of this world could keep us apart, because this\nis not my world. You are! I think it is finally time to say, I LOVE YOU!\nLove,\nKarl <3"
+        print "*I ... I cant belive this. Sophia ... my love ...*"
     elif letter == 4:
-        print "Letter #4"
+        print "My Love!\nYou are someone who makes love feel easy. I cannot express my deepest emotions in\nwords. You are everything to me. Being in love is a strange thing, my thoughts constantly\ndrift towards you. My stomach is in a constant state of butterflies. I have completely\nfallen for you. Everything you do, everything you say, everything you are. You are my\nfirst thought in the morning and the last thought I have before bed. And every thought\ninbetween ... you're there too.\nCan't wait to come by later,\nXOXO Karl"
+        print"*This is the final straw! The second she gets home she will hear what I have to think!\nHow dare she ... the bond we have ...*"
+
 
 print"-----------------------------------------"
 print"            Wake up Simon"
@@ -166,7 +286,8 @@ print"-----------------------------------------"
 print"    you wake up in the hallway. why did you fall asleep here? you stand up and stretch."
 print"Where is Sophia? you need to find out where she went. better investigate.              "
 energy=100
-letterCount = 1;
+letterCount = 1
+boolGlobal = True
 hallway()
 
 
